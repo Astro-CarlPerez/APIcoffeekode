f4112154129a63b2c8b86927d340c83c60098d00
astro-carlperez <carl.perec97@gmail.com>
2022-12-22 17:09:08 -0600

    se añaden los archivos de la BD

---
+++ src/main/java/org/generation/CoffeeKode/CoffeeKodeApplication.java
@@ -0,0 +1,13 @@
+package org.generation.CoffeeKode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeKodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeKodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/generation/CoffeeKode/CoffeeKodeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.CoffeeKode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeKodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
