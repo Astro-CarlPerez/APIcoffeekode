eb6f1d42a9014549f1042f0fd8dfc987ccf3f564
astro-carlperez <carl.perec97@gmail.com>
2022-12-23 12:34:06 -0600

    Se suben cambios finales de Spring 22/12/22

--- src/main/java/org/generation/CoffeeKode/UsersController.java
+++ src/main/java/org/generation/CoffeeKode/UsersController.java
@@ -18,44 +18,45 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping(path= "/api/CoffeeKode/usuarios")
+@RequestMapping(path= "/api/CoffeeKode/users")
 @CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE })
 
-public class UserController {
+public class UsersController {
+
 
 	//constante para utilizar el autowired (para crear una instancia del tipo Usuario aqui adentro, y reconocer todos los metodos que implemente la union de User+ServiceUser) BASICAMENTE ES DECIRLE AL CONTROLLER LO QUE PUEDE HACER EL USUARIO (SERVICE)
 
-	private final UserService userService;
+	private final UsersService usersService;
 	
 	
 	@Autowired
-	public UserController(UserService userService) {
-		this.userService = userService;
+	public UsersController(UsersService usersService) {
+		this.usersService = usersService;
 	}
 		
 	//GET para traer usuarios
 	@GetMapping
-	public List<User> getUsuarios(){
-		return userService.getUsers();
+	public List<Users> getUsuarios(){
+		return usersService.getUsers();
 	}//getUsuarios
 
 	//GET para traer un usuario por Id
 	@GetMapping(path="{userId}")
-	public User getUsuario(@PathVariable ("userId") Long userId) {
-		return userService.getUser(userId);
+	public Users getUsuario(@PathVariable ("userId") Long userId) {
+		return usersService.getUser(userId);
 	}//getUsuario
 	
 	
 	//DELETE para borrar un usuario por id
 	@DeleteMapping(path="{userId}")
 	public void deleteUsuario(@PathVariable ("userId") Long userId) {
-		userService.deleteUser(userId);
+		usersService.deleteUser(userId);
 	}//deleteUsuario
 
 	//POST para agregar un nuevo usuario
 	@PostMapping
-	public void postUsuario(@RequestBody User user) {
-		userService.addUser(user);
+	public void postUsuario(@RequestBody Users users) {
+		usersService.addUser(users);
 	}//postUsuario
 	
 	//PUT para modificar un usuario ya existente, por medio de su id
@@ -63,8 +64,12 @@ public class UserController {
 	public void updateUsuario (@PathVariable ("userId") Long userId,
 			@RequestParam String currentPassword,
 			@RequestParam String newPassword) {	
-		userService.updateUser(userId, currentPassword, newPassword);
+		usersService.updateUser(userId, currentPassword, newPassword);
 	}
 
 }
+
+
+	
+	
 
